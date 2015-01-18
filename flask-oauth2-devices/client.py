ae6680a8d644744f338d9ebbec1d631cc46a9a31
Joe Cabrera <calcmaster16@gmail.com>
2015-01-18 17:11:26 -0500

    fleshing out the access_token object

--- flask-oauth2-devices/client.py
+++ flask-oauth2-devices/client.py
@@ -69,8 +69,8 @@ class OAuth2devices(object):
         The authorization server MUST authenticate the client. 
         """
 
-        if self.request.method is "POST":   
-            headers = {'Allow': 'POST'}
+        if self.request.method is "POST":
+            self.request.headers = {'Allow': 'POST'}
             self.request.status_code = 405
 
         if getApp() is None:
@@ -83,7 +83,7 @@ class OAuth2devices(object):
         auth_code = AuthorizationCode(app_id, user_id, scope, expires_one)
         auth_code.create_new_code()
 
-        return create_oauth2_response(authorize_link, activate_link, expires_interval, polling_interval)
+        return create_oauth2_code_response(authorize_link, activate_link, expires_interval, polling_interval)
 
     def authorize(self, callback=None, state=None, **kwargs):
         """
@@ -103,11 +103,45 @@ class OAuth2devices(object):
                 "device_code: "c8fe9de9e6c5f80bc543c492aaa2fbaf2b081601"
             }
         """
-        params = dict(self.request_token_params) or {}
-        params.update(**kwargs)
 
+        if self.request.method is "POST":
+            self.request.headers = {'Allow': 'POST'}
+            self.request.status_code = 405
 
-    def create_oauth2_response(authorize_link=None, activate_link=None, expires_interval=0, polling_interval=0):
+        data = requests.json()
+        auth_code = AuthorizationCode().load(data['auth_code'])
+
+        if auth_code is None:
+            raise OAuth2Exception(
+                'This token could not be found',
+                type='invalid_token'
+            )
+
+        if auth_code.expires_on is None and auth_code.expires_on < datetime.datetime.now():
+            raise OAuth2Exception(
+                'Authorization code has expired',
+                type='invalid_token'
+            )
+
+        device_code = data['device_code']
+
+        if auth_code.device_code is False:
+            raise OAuth2Exception(
+                'You have not authorized this device code yet',
+                type='not_authorized'
+            )
+
+        if auth_code.get_device_code() != device_code:
+            raise OAuth2Exception(
+                'Your auth code does not match the device',
+                type='invalid_token'
+            )
+
+        access_token = auth_code.exchange_for_access_token()
+
+        return create_oauth2_code_response(authorize_link, activate_link, expires_interval, polling_interval)
+
+    def create_oauth2_code_response(authorize_link=None, activate_link=None, expires_interval=0, polling_interval=0):
         """
         The authorization server issues an device code which the device will have
         prompt the user to authorize before following the activate link to
@@ -155,12 +189,91 @@ class OAuth2devices(object):
             }
         """
         self.response = urllib3.response.HTTPResponse({
-            'device_code' : auth_code.getDeviceCode(),
+            'device_code' : auth_code.get_device_code(),
             'authorize_code ': auth_code.code,
             'authorize_link': authorize_link,
             'activate_link': activate_link,
             'expires_in': expires_interval,
             'interval': polling_interval}, {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-store',
+            'Pragma': 'no-cache'}
+
+        return self.response
+
+    def create_oauth2_token_response(access_token=None):
+        """
+        The authorization server issues an access token and optional refresh
+        token, and constructs the response by adding the following parameters
+        to the entity-body of the HTTP response with a 200 (OK) status code:
+
+        access_token
+            REQUIRED.  The access token issued by the authorization server.
+
+        token_type
+            REQUIRED.  The type of the token issued as described in
+            Section 7.1.  Value is case insensitive.
+
+        expires_in
+            RECOMMENDED.  The lifetime in seconds of the access token.  For
+            example, the value "3600" denotes that the access token will
+            expire in one hour from the time the response was generated.
+            If omitted, the authorization server SHOULD provide the
+            expiration time via other means or document the default value.
+
+        refresh_token
+            OPTIONAL.  The refresh token, which can be used to obtain new
+            access tokens using the same authorization grant as described
+            in Section 6.
+
+        scope
+            OPTIONAL, if identical to the scope requested by the client;
+            otherwise, REQUIRED.  The scope of the access token as
+            described by Section 3.3.
+
+        The parameters are included in the entity-body of the HTTP response
+        using the "application/json" media type as defined by [RFC4627].  The
+        parameters are serialized into a JavaScript Object Notation (JSON)
+        structure by adding each parameter at the highest structure level.
+        Parameter names and string values are included as JSON strings.
+        Numerical values are included as JSON numbers.  The order of
+        parameters does not matter and can vary.
+
+        The authorization server MUST include the HTTP "Cache-Control"
+        response header field [RFC2616] with a value of "no-store" in any
+        response containing tokens, credentials, or other sensitive
+        information, as well as the "Pragma" response header field [RFC2616]
+        with a value of "no-cache".
+
+        For example:
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json;charset=UTF-8
+            Cache-Control: no-store
+            Pragma: no-cache
+
+            {
+                "access_token":"2YotnFZFEjr1zCsicMWpAA",
+                "token_type":"example",
+                "expires_in":3600,
+                "refresh_token":"tGzv3JOkF0XG5Qx2TlKWIA",
+                "example_parameter":"example_value"
+            }
+
+        The client MUST ignore unrecognized value names in the response.  The
+        sizes of tokens and other values received from the authorization
+        server are left undefined.  The client should avoid making
+        assumptions about value sizes.  The authorization server SHOULD
+        document the size of any value it issues.
+
+        http://tools.ietf.org/html/rfc6749#section-5.1
+        """
+        self.response = urllib3.response.HTTPResponse({
+            'access_token' : access_token,
+            'token_type ': access_token.token_type,
+            'scope': authorize_link,
+            'expires_in': expires_interval,
+            'refresh_token': polling_interval}, {
             'Content-Type': 'application/json',
             'Cache-Control': 'no-store',
             'Pragma': 'no-cache'}
@@ -189,6 +302,77 @@ class OAuth2devices(object):
                     type='invalid_secret'
                 )
 
+class AccessToken():
+    """
+    +--------+                                           +---------------+
+    |        |--(A)------- Authorization Grant --------->|               |
+    |        |                                           |               |
+    |        |<-(B)----------- Access Token -------------|               |
+    |        |               & Refresh Token             |               |
+    |        |                                           |               |
+    |        |                            +----------+   |               |
+    |        |--(C)---- Access Token ---->|          |   |               |
+    |        |                            |          |   |               |
+    |        |<-(D)- Protected Resource --| Resource |   | Authorization |
+    | Client |                            |  Server  |   |     Server    |
+    |        |--(E)---- Access Token ---->|          |   |               |
+    |        |                            |          |   |               |
+    |        |<-(F)- Invalid Token Error -|          |   |               |
+    |        |                            +----------+   |               |
+    |        |                                           |               |
+    |        |--(G)----------- Refresh Token ----------->|               |
+    |        |                                           |               |
+    |        |<-(H)----------- Access Token -------------|               |
+    +--------+           & Optional Refresh Token        +---------------+
+
+
+    The flow illustrated in Figure 2 includes the following steps:
+
+     (A)  The client requests an access token by authenticating with the
+          authorization server, and presenting an authorization grant.
+     (B)  The authorization server authenticates the client and validates
+          the authorization grant, and if valid issues an access token and
+          a refresh token.
+     (C)  The client makes a protected resource request to the resource
+          server by presenting the access token.
+     (D)  The resource server validates the access token, and if valid,
+          serves the request.
+     (E)  Steps (C) and (D) repeat until the access token expires.  If the
+          client knows the access token expired, it skips to step (G),
+          otherwise it makes another protected resource request.
+     (F)  Since the access token is invalid, the resource server returns
+          an invalid token error.
+     (G)  The client requests a new access token by authenticating with
+          the authorization server and presenting the refresh token.  The
+          client authentication requirements are based on the client type
+          and on the authorization server policies.
+     (H)  The authorization server authenticates the client and validates
+          the refresh token, and if valid issues a new access token (and
+          optionally, a new refresh token).
+
+    Steps C, D, E, and F are outside the scope of this specification
+    This access token is granted as a Bearer token, as defined by http://tools.ietf.org/html/draft-ietf-oauth-v2-bearer-22
+    """
+    def __init__(app_id=0, user_id=0, scope=None, expires_on=None):
+        self.app_id = app_id
+        self.user_id = user_id
+        self.scope = scope
+        self.expires_on = expires_on
+
+    def refresh(token):
+        token = AccessToken()
+        token.user_id = self.user_id
+        token.app_id = self.app_id
+        token.grant_type = self.grant_type
+        token.token_type = self.token_type
+        token.refresh_token = create_new_refresh_token()
+
+    def create_new_token():
+        return hashlib.md5("app:" + self.app_id + ":user:" + self.user_id + ":" + pyOpenSSL.rand())
+
+    def create_new_refresh_token():
+        return hashlib.sha1("app:" + self.app_id + ":user:" + ":token:" + self.id)
+
 
 class AuthorizationCode():
 
@@ -207,7 +391,7 @@ class AuthorizationCode():
         access_token = 
 
     def get_device_code():
-        return hmac.new(self.key, 'secret:'.self.id, 'sha1')
+        return hmac.new(OUR_KEY, 'secret:'.self.id, 'sha1')
 
     def create_new_code():
         self.code = hashlib.sha1("secret:" + self.app_id + ":req:" + pyOpenSSL.rand())
@@ -216,6 +400,7 @@ class AuthorizationCode():
 
         if self.expires_on is None:
             self.expires_on = datetime.datetime.now().date() + AUTH_EXPIRATION_TIME
+
 
 class OAuth2Exception(RuntimeError):
     def __init__(self, message, type=None, data=None):
