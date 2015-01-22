42f8df7f37b05aa1e35f3fdc986203da7677977a
Joe Cabrera <calcmaster16@gmail.com>
2015-01-21 21:51:12 -0500

    reduce reliance on other libraries, added six

--- flask-oauth2-devices/utility.py
+++ flask-oauth2-devices/utility.py
@@ -1,7 +1,31 @@
+import six
 import base64
 from datetime import datetime
 from flask import request, Response
-from oauthlib.common import to_unicode, bytes_type
+
+if six.PY3:
+    bytes_type = bytes
+else:
+    bytes_type = str
+
+def to_unicode(data):
+    """Convert utf-8 to unicode."""
+    if isinstance(data, bytes_type):
+        return unicode_type(data, 'UTF-8')
+
+    if hasattr(data, '__iter__'):
+        try:
+            dict(data)
+        except TypeError:
+            pass
+        except ValueError:
+            return (to_unicode(i, 'UTF-8') for i in data)
+        else:
+            if hasattr(data, 'items'):
+                data = data.items()
+            return dict(((to_unicode(k, 'UTF-8'), to_unicode(v, 'UTF-8')) for k, v in data))
+
+    return data
 
 def to_bytes(text, encoding='utf-8'):
     """Make sure text is bytes type."""
@@ -11,13 +35,13 @@ def to_bytes(text, encoding='utf-8'):
         text = text.encode(encoding)
     return text
 
-def decode_base64(text, encoding='utf-8'):
-    """Decode base64 string."""
+def decode_base64(text):
+    """Decode base64 string from utf-8."""
     text = to_bytes(text, encoding)
-    return to_unicode(base64.b64decode(text), encoding)
+    return to_unicode(base64.b64decode(text))
 
 def create_response(headers, body, status):
-    """Create response class for Flask."""
+    """Create response object for Flask."""
     response = Response(body or '')
     for k, v in headers.items():
         response.headers[str(k)] = v
@@ -27,7 +51,6 @@ def create_response(headers, body, status):
 
 def json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
-
     if isinstance(obj, datetime):
         serial = obj.isoformat()
         return serial
